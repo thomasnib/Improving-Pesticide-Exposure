6a8b98d3b0562930bff87821179c43826b92c9aa
thomasnib <117747807+thomasnib@users.noreply.github.com>
2023-06-20 12:10:41 +0200

    update

---
+++ Scripts/3. Spatial Interpolation/3.3 Spatial Interpolation - UK.R
@@ -0,0 +1,149 @@
+################################################################################
+# Title: 3.3 Spatial Interpolation - Universal Kriging
+# Author: Thomas Nibbering
+# Date: June 19th, 2023
+# Version: V1
+################################################################################
+#                               1. Initialisation
+################################################################################
+####
+# 1. Load Packages
+####
+# Load packages
+library(sf)
+library(terra)
+library(automap)
+library(tidyverse)
+
+####
+# 2. Load Data
+####
+# Load wind directions
+wind <- fread('/Users/thomasnibbering/Documents/Github/Improving-Pesticide-Exposure/Data/3. Final/KNMI_Station_Wind_Directions.csv')
+
+# Load Dutch country boundary
+nld <- st_read('/Users/thomasnibbering/Documents/Github/Improving-Pesticide-Exposure/Data/1. External/NL_Country_Boundary.gpkg')
+
+####
+# 3. Regular Grid
+####
+# Obtain regular grid (1x1km)
+grd <- st_bbox(nld) %>%
+       st_as_stars(dx = 1000, dy = 1000) %>%
+       st_set_crs(28992) %>%
+       st_crop(nld) %>%
+       st_as_sf() %>%
+       st_centroid() %>%
+       cbind(., st_coordinates(.))
+
+####
+# 4. Scenario
+####
+# Define scenario 
+scenario <- wind %>% filter(datetime == as.POSIXct('2017-04-04 12:00:00', tz = 'UTC'))
+
+####
+# 5. Functions
+####
+# Define circular root-mean-squared error function
+CRMSE <- function(actual, predicted){
+  # Obtain difference 
+  diff <- abs(actual - predicted)
+  # Obtain circular difference
+  circ <- ifelse(diff > 180, 360 - diff, diff)
+  # Obtain circular root-mean-squared error
+  CRMSE <- sqrt(mean(circ^2))
+  return(CRMSE)
+}
+
+# Define leave-one-out cross validation function
+loocv_uk <- function(data){
+            # Obtain folds
+            data <- mutate(data, ID = seq.int(nrow(data)))
+            # Initialise error metric
+            error <- c()
+            # Iterate over folds
+            for (i in 1:nrow(data)){
+              # Obtain Train/Validation
+              train <- filter(data, !ID %in% i)
+              validate <- filter(data, ID %in% i)
+              # Convert to sf-object
+              st_train <- st_as_sf(train, coords = c('X', 'Y'), crs = 28992)
+              st_validate <- st_as_sf(validate, coords = c('X', 'Y'), crs = 28992)
+              # Obtain Coordinates
+              st_train <- st_train %>% cbind(., st_coordinates(.))
+              st_validate <- st_validate %>% cbind(., st_coordinates(.))
+              # Semi-variogram fit
+              m <- autofitVariogram(formula = DD ~ X + Y, input_data = st_train)
+              # Model fit
+              uk <- krige(formula = DD ~ X + Y, st_train, st_validate, model = m$var_model)$var1.pred
+              # Model performance
+              crmse <- CRMSE(st_validate$DD, uk)
+              # Model output
+              error <- c(crmse, error)
+            }
+            return(mean(error))
+}
+
+################################################################################
+#                       2. Universal Kriging Interpolation
+################################################################################
+####
+# 1. LOOCV Universal Kriging
+####
+# Perform leave-one-out cross validation using universal kriging algorithm
+loocv_uk(scenario)
+
+####
+# 2. Spatial Interpolation
+####
+# Convert dataframe to sf-object
+scenario <- st_as_sf(scenario, coords = c('X', 'Y'), crs = 28992)
+
+# Obtain coordinates
+scenario <- scenario %>% cbind(., st_coordinates(.))
+
+# Obtain semi-variogram
+m <- autofitVariogram(DD ~ X + Y, scenario)
+
+# Define universal kriging algorithm
+uk <- krige(formula = DD ~ X + Y, scenario, grd, model = m$var_model)
+
+################################################################################
+#                       3. Universal Kriging Visualisation
+################################################################################
+####
+# 1. Visualise
+####
+# Visualise universal kriging Interpolation
+uk_visual <- tm_shape(uk) + 
+             tm_dots(col = 'var1.pred',
+                     breaks = c(0, 40, 80, 120, 160, 200, 240, 280, 320, 360),
+                     palette = c('#5287c6', '#436fac', '#345792', '#254179',
+                                 '#152c60', 
+                                 '#254179', '#345792', '#436fac', '#5287c6'),
+                     legend.show = F) + 
+              tm_add_legend(type = 'fill',
+                            title = 'Wind Direction (in Degrees)',
+                            col = c('#5287c6', '#436fac', '#345792', '#254179',
+                                    '#152c60', 
+                                    '#254179', '#345792', '#436fac', '#5287c6'),
+                            labels = c('0 to 40', '40 to 80', '80 to 120', '120 to 160',
+                                       '160 to 200',
+                                       '200 to 240', '240 to 280', '280 to 320', '320 to 360')) + 
+              tm_compass() + 
+              tm_scale_bar(width = 0.15) + 
+              tm_layout(frame = F, 
+                        legend.title.fontfamily = 'Times New Roman',
+                        legend.title.fontface = 'bold',
+                        legend.title.size = 1, 
+                        legend.text.fontfamily = 'Times New Roman',
+                        legend.text.size = 0.8)
+
+####
+# 2. Store
+####
+# Store visualisation in PNG-format
+tmap_save(uk_visual, '/Users/thomasnibbering/Documents/Github/Improving-Pesticide-Exposure/Thesis/Figures/UK_Interpolation.png')
+
+
