06617f654055dbfc5b89332d6d05a33303785c61
thomasnib <117747807+thomasnib@users.noreply.github.com>
2023-07-05 20:23:53 +0200

    update

--- Scripts/3. Spatial Interpolation/Untitled.R
+++ Scripts/3. Spatial Interpolation/Untitled.R
@@ -1,5 +1,5 @@
 ################################################################################
-# Title: 4.1 Data Visualisations
+# Title: 4.1 Data Visualisations - Poster
 # Author: Thomas Nibbering
 # Date: June 14th, 2023
 # Version: V1
@@ -41,65 +41,30 @@ wind_direction <- ggplot(wind_stns, aes(x = DD)) +
                   geom_histogram(binwidth = 20, fill = '#FFCD00', color = '#454545', linewidth = 0.15) + 
                   scale_x_continuous(breaks = c(0, 90, 180, 270), limits = c(0, 360)) + 
                   coord_polar(start = 0) + 
-                  labs(y = 'Prevalence', 
+                  labs(title = 'Hourly Wind Direction Distribution in 2017',
+                       y = 'Prevalence', 
                        x = 'Wind Direction (in Degrees)') + 
                   theme_fira() + 
                   theme(text = element_text(family = 'Times New Roman'),
+                        title = element_text(hjust = 0.5, face = 'bold'),
                         axis.line.x = element_blank(),
                         axis.title.x = element_text(hjust = 0.5, face = 'bold'), 
                         axis.line.y = element_blank(),
                         axis.title.y = element_text(hjust = 0.5, face = 'bold'))
 
-# Store wind directions
-ggsave('/Users/thomasnibbering/Documents/Github/Improving-Pesticide-Exposure/Thesis/Figures/Wind_Directions.png', 
-       wind_direction, 
-       height = 3, 
-       width = 7)
-
-####
-# 2. Environment
-####
-# Remove objects from environment
-rm(wind_direction)
-  
-################################################################################
-#                           2. Meteorological Stations
-################################################################################
-####
-# 1. Stations
-####
-# Obtain coordinates for each distinct station considered
-stns <- wind_stns %>% distinct(station_code, .keep_all = T)
-
-# Transform into sf-object
-stns <- st_as_sf(stns, coords = c('X', 'Y'), crs = 28992)
-
-# Visualise stations
-stations <- tm_shape(nld) + 
-            tm_polygons(col = '#454545') + 
-            tm_shape(stns) + 
-            tm_dots(size = 0.3, col = '#FFCD00') +
-            tm_compass() + 
-            tm_layout(frame = F) + 
-            tm_scale_bar(width = 0.2)
-
-# Store stations
-tmap_save(stations, '/Users/thomasnibbering/Documents/Github/Improving-Pesticide-Exposure/Thesis/Figures/KNMI_Stations.png')
-
-####
-# 2. Environment
-####
-# Remove objects from environment
-rm(stns, stations)
+# Store Wind Directions
+ggsave('~/Desktop/Wind_Direction.png',
+       width = 6, 
+       height = 4)
 
 ################################################################################
-#                           3. Sensitivity Analysis
+#                           2. Sensitivity Analysis
 ################################################################################
 ####
 # 1. Sensitivity Analysis
 ####
 # Define algorithm associated with each analysis
-sen_m$algorithm <- "Mode Interpolation"
+sen_m$algorithm <- 'Mode Interpolator'
 sen_nn$algorithm <- 'Nearest Neighbour'
 sen_idw$algorithm <- 'Inverse Distance Weighting'
 sen_uk$algorithm <- 'Universal Kriging'
@@ -110,15 +75,21 @@ sensitivity <- rbind(sen_m, sen_nn, sen_idw, sen_uk, sen_rf)
 
 # Visualise sensitivity analysis
 sen_visual <- ggplot(sensitivity, aes(x = CRMSE, y = reorder(algorithm, CRMSE, median))) + 
-              geom_boxplot() + 
-              labs(x = 'Circular Root-Mean-Squared Error (CRMSE)', 
+              geom_boxplot() +
+              labs(title = 'Comparison of Spatial Interpolator Out-of-Sample Performance',
+                   subtitle = 'on Hourly Wind Direction Records (N = 383)',
+                   x = 'Circular Root-Mean-Squared Error (CRMSE)', 
                    y = '') + 
               theme_fira() + 
               theme(text = element_text(family = 'Times New Roman'),
-                    axis.title.x = element_text(hjust = 0.5, face = 'bold'))
+                    title = element_text(hjust = 0.5, face = 'bold'), 
+                    axis.title.x = element_text(hjust = 0.5, face = 'bold'), 
+                    axis.text.y = element_text(face = 'bold'))
 
-# Store sensitivity analysis 
-ggsave('/Users/thomasnibbering/Documents/Github/Improving-Pesticide-Exposure/Thesis/Figures/Sensitivity_Analysis.png', 
-       sen_visual,
-       height = 3,
-       bg = 'white')
+sen_visual
+
+# Store sensitivty analysis
+ggsave('~/Desktop/Sensitivity_Analysis.png', 
+       width = 10,
+       height = 7)
+
